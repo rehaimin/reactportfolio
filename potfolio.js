1d002ec0aad55fa1d42ee3c50c919611e091870c
Mohammed REHAIMIN <mrehaimin@gmail.com>
2022-04-19 19:03:25 +0000

    Updates

---
+++ potfolio.js
@@ -0,0 +1,43 @@
+const navCol = document.querySelector(".nav");
+const nav = document.querySelector("nav");
+const myProjectsBtn = document.querySelector(".perso a");
+
+const menuBtn = document.querySelector(".menu-btn");
+
+menuBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	navCol.classList.toggle("nav-display");
+});
+
+nav.addEventListener("click", () => {
+	if (window.innerWidth <= 870) {
+		if (navCol.classList.contains("nav-display")) {
+			navCol.classList.remove("nav-display");
+		}
+	}
+});
+
+function scrollToTop() {
+	const mainContainer = document.querySelector(".main-container");
+	mainContainer.scrollTo(0, 0);
+}
+
+const scrollTop = document.querySelector(".scrollTop");
+scrollTop.addEventListener("click", (e) => {
+	e.preventDefault();
+	scrollToTop();
+});
+
+const themeBtn = document.querySelector(".theme-btn");
+themeBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	if (getCssVariable("--bg") == darkTheme.bg) {
+		toggleTheme(lightTheme);
+		themeBtn.innerHTML = "&#xF494;";
+		localStorage.setItem("theme", "light");
+	} else {
+		toggleTheme(darkTheme);
+		themeBtn.innerHTML = "&#xF5A1;";
+		localStorage.setItem("theme", "dark");
+	}
+});
