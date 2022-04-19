9178a65785153722dc15b14ebf0a28302491ecd3
Mohammed REHAIMIN <mrehaimin@gmail.com>
2022-04-19 19:10:26 +0000

    Updates

--- precache-manifest.a177449bbf8bf7ff2afa2264078246dd.js
+++ precache-manifest.a177449bbf8bf7ff2afa2264078246dd.js
@@ -60,12 +60,12 @@ self.__precacheManifest = [
     "url": "/reactportfolio/static/media/bootstrap-icons.a892cbb9.woff"
   },
   {
-    "revision": "08c20a487911694291bd8c5de41315ad",
-    "url": "/reactportfolio/static/media/Poppins-Bold.08c20a48.ttf"
-  },
-  {
     "revision": "093ee89be9ede30383f39a899c485a82",
     "url": "/reactportfolio/static/media/Poppins-Regular.093ee89b.ttf"
+  },
+  {
+    "revision": "08c20a487911694291bd8c5de41315ad",
+    "url": "/reactportfolio/static/media/Poppins-Bold.08c20a48.ttf"
   },
   {
     "revision": "5067a6c4ca23ec58aa6ef46b365a97dc",
