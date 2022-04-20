bc35b6ad95ca0f9e9200e9219230ea2b87e2fc4c
Mohammed REHAIMIN <mrehaimin@gmail.com>
2022-04-20 11:02:20 +0000

    BrowserRouter moved to index.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,19 @@ import Projects from "./components/Projects";
 import Blog from "./components/Blog";
 import Scrolltop from "./components/Scrolltop";
 import Contact from "./components/Contact";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="main-container">
 			<Header />
 			<main>
-				<BrowserRouter>
-					<Routes>
-						<Route path="/" element={<Perso />} />
-						<Route path="/projects" element={<Projects />} />
-						<Route path="/blog" element={<Blog />} />
-						<Route path="/contact" element={<Contact />} />
-					</Routes>
-				</BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Perso />} />
+					<Route path="/projects" element={<Projects />} />
+					<Route path="/blog" element={<Blog />} />
+					<Route path="/contact" element={<Contact />} />
+				</Routes>
 			</main>
 			<Scrolltop />
 		</div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,33 +4,33 @@ export default class Header extends Component {
 	render() {
 		return (
 			<header>
-				<div class="container">
-					<div class="flex" id="header">
-						<div class="col-6 flex space-between align-items-center title">
+				<div className="container">
+					<div className="flex" id="header">
+						<div className="col-6 flex space-between align-items-center title">
 							<div>
 								<h1>&lt;Mohammed REHAIMIN/&gt;</h1>
 							</div>
-							<a href="/" role="button" class="theme-btn">
+							<a href="/" role="button" className="theme-btn">
 								&#xF494;
 							</a>
-							<a href="/" role="button" class="menu-btn" id="menuBtn">
+							<a href="/" role="button" className="menu-btn" id="menuBtn">
 								&#xF479;
 							</a>
 						</div>
-						<div class="col-6 nav">
+						<div className="col-6 nav">
 							<nav>
-								<ul class="flex">
+								<ul className="flex">
 									<li>
 										<Link to="/">Home</Link>
 									</li>
 									<li>
-										<Link to="/projects">My projects</Link>
+										<Link to="projects">My projects</Link>
 									</li>
 									<li>
-										<Link to="/blog">Blog</Link>
+										<Link to="blog">Blog</Link>
 									</li>
 									<li>
-										<Link to="/contact">Contact me</Link>
+										<Link to="contact">Contact me</Link>
 									</li>
 								</ul>
 							</nav>
